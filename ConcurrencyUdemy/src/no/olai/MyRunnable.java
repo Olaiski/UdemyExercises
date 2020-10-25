6b3eab55b8ca7b35c29184d838d17f0c7b192af9
Olaiski <kungbinders@gmail.com>
2020-10-25 21:37:18 +0100

    Concurrency, thread program.

---
+++ ConcurrencyUdemy/src/no/olai/MyRunnable.java
@@ -0,0 +1,12 @@
+package no.olai;
+
+import static no.olai.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+
+}
